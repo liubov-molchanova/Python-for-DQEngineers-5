e649effdba1a95298ce2da13b85fada64f4ae3ed
Liubov Molchanova <liubov.molchanova@mongodb.com>
2022-03-02 11:34:08 +0300

    Update 6_Module. Files.py
    HW 6 updated

--- 6_Module. Files.py
+++ 6_Module. Files.py
@@ -1,6 +1,8 @@
 import re
 import os
 import sys
+import string
+import csv
 from datetime import date, datetime, timedelta
 from Functions import normal_text
 
@@ -10,26 +12,41 @@ class First_choice:
         if self.choice == '1':
             self.publication_type = input('''Please, choose a Publication type:\n1 - for a news\n2 - for an ads\n3 - for a healthy breakfast: \n''')
         elif self.choice == '2':
-            self.document_path = sys.path[1] # the folder where our Python project is stored
-            self.document_name = input('Please, provide your document name: \n')
+            self.which_document = input('If you want to use your document, please, enter 1. \nIf you want to use a default file, please, enter 2. \n')
+            if self.which_document == '1':
+                self.document_name = input("Please, enter your document name (including '.txt'- (example: document.txt)): \n")
+                try:
+                    file = open(self.document_name)
+                    file.close()
+                except FileNotFoundError:
+                    print("Wrong file name!")
+                    quit()
+            elif self.which_document == '2':
+                 self.document_name = 'Final.txt'
         else:
             print('Wrong choice! Try again.')
 
-    def source_document(self): # we are creating a full path to the document in a default folder and splitting it by double newlines
-        self.source_file = os.path.join(self.document_path, self.document_name)
-        self.source_file_open = open(self.source_file, 'r').read()
+    def source_document(self):
+        self.source_file_open = open(self.document_name, 'r').read()
         self.text = re.split('\\n\\n', self.source_file_open)
         return self.text
 
     def final_document(self, doc_name = 'Final.txt'): # we are inserting information from the file provided to our new file
-        self.publications_number = int(input('Please, define how many publications you want to use: \n'))
-        f = open('Final.txt', 'a')
-        if self.publications_number > 0:
-            for i in self.text:
-                if self.text.index(i) < self.publications_number:
+        try:
+            self.publications_number = int(input('Please, define how many publications you want to use: \n'))
+            f = open('Final.txt', 'a')
+            if self.publications_number < len(self.text):
+                if self.publications_number > 0:
+                    for i in self.text:
+                        if self.text.index(i) < self.publications_number:
+                            f.write(i + '\n\n')
+            elif self.publications_number >= len(self.text):
+                for i in self.text:
                     f.write(i + '\n\n')
-        f.close()
-        os.remove(self.source_file)
+                os.remove(self.document_name)
+            f.close()
+        except ValueError:
+            print('Wrong symbol! Not a digit!')
 
 class Publication:
     def __init__(self): # here we are using our normalization from the HW#4
@@ -81,5 +98,11 @@ def publication():
         source_type.final_document()
     else:
         print("Sorry. We have no other publication input type")
+        publication()
+    last_choice = input("If you want to end the program, enter 1.\nIf you want to continue, enter 2.\n")
+    if last_choice == '1':
+        csv_write()
+    elif last_choice == '2':
+        publication()
 
 publication()
